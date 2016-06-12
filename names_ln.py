bb3f7c445a5c54f36fc13268255cbf2bfc2f442f
Walter Huf <hufman@gmail.com>
2016-06-11 20:08:02 -0500

    Found a few more names from mus_prog_1

--- names_ln.py
+++ names_ln.py
@@ -10,4 +10,6 @@ if len(sys.argv)>2:
   offset = int(sys.argv[2], 16)
 
 for i,name in enumerate(file.readlines()):
+  if name.strip() == '':
+    continue
   print('%08x.wav %s.wav' % (offset+i, name.strip()))
